9f12b6689dceea40e394dce98e20cbf758d98f72
kianorr <60586957+kianorr@users.noreply.github.com>
2021-08-13 11:24:44 -0700

    Update server.py

--- server.py
+++ server.py
@@ -61,27 +61,3 @@ class Server(SpecInfo):
         self.sock.sendto(b"%a" % self.shape, (self.address[0], self.address[1]))
         
         self.sock.close()
-
-# Server class parameters
-group = '224.1.1.1'
-port = 5004
-address = [group, port]
-ttl = 2
-chunkSize = 10
-
-# Initializing ReceiveParameters methods
-p = ReceiveParameters(address)
-p.sockets_Receive()
-params = p.receiveParameters()
-
-# SpecInfo class parameters
-# From client.py, SpecInfo_parameters = [trigVal, intTime, deviceNum]
-trigVal = params[0]
-intTime = params[1]
-deviceNum = params[2]
-
-# Initializing Server methods
-s = Server(chunkSize, address, ttl, trigVal, intTime, deviceNum)
-s.sockets_Send()
-s.getData()
-s.sendData()
