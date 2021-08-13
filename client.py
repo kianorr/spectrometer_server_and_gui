66780269f93ce669ddbfd216a7041058038ee50e
kianorr <60586957+kianorr@users.noreply.github.com>
2021-08-13 11:26:18 -0700

    Update client.py

--- client.py
+++ client.py
@@ -59,25 +59,4 @@ class Receive:
         specDecompressed = zlib.decompress(self.bSpectrum)
         shape = tuple(json.loads(self.bShape))
         s = np.frombuffer(specDecompressed).reshape(shape)
-        return shape
-
-# Choosing spectrometer settings
-trigVal = 0        # value of trigger mode
-intTime = 1000     # integration time in microseconds
-deviceNum = 0      # element value for device list (if there's only one device, then it's zero)
-
-# Address parameters
-group = '224.1.1.1'
-port = 5004
-address = [group, port]
-
-# Intitializing SendParameters class
-s = SendParameters(address, trigVal, intTime, deviceNum)
-s.sendParams()
-
-# Initializing Receive class
-# Not sure if there's a better way to do this
-r = Receive(address)
-r.sockets_Receive()
-r.receiveData()
-r.prepareData()
+        return s
