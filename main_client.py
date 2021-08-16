e7a929c7df14e080bc7f2782cbc2cf7e80dc48a2
kianorr <60586957+kianorr@users.noreply.github.com>
2021-08-15 23:08:28 -0700

    Update main_client.py

--- main_client.py
+++ main_client.py
@@ -1,9 +1,9 @@
 from client import SendParameters, Receive
 
 # Choosing spectrometer settings
-trigVal = 0        # value of trigger mode
-intTime = 1000     # integration time in microseconds
-deviceNum = 0      # element value for device list (if there's only one device, then it's zero)
+trig_val = 0        # value of trigger mode
+int_time = 1000     # integration time in microseconds
+device_num = 0      # element value for device list (if there's only one device, then it's zero)
 
 # Address parameters
 group = '224.1.1.1'
@@ -11,12 +11,11 @@ port = 5004
 address = [group, port]
 
 # Intitializing SendParameters class
-s = SendParameters(address, trigVal, intTime, deviceNum)
-s.sendParams()
+s = SendParameters(address, trig_val, int_time, device_num)
+s.send_parameters()
 
 # Initializing Receive class
-# Not sure if there's a better way to do this
 r = Receive(address)
-r.sockets_Receive()
-r.receiveData()
-print(r.prepareData())
+r.set_socket_receive()
+r.receive_data()
+print(r.prepare_data())
