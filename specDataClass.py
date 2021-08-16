1eabe5206ad4767487b27afebd5e8d3444861f5c
kianorr <60586957+kianorr@users.noreply.github.com>
2021-08-15 22:56:08 -0700

    Update specDataClass.py

--- specDataClass.py
+++ specDataClass.py
@@ -3,38 +3,90 @@ import zlib
 import numpy as np
 
 class SpecInfo(Spectrometer):
-    def __init__(self, trigVal, intTime, deviceNum):
-        device = list_devices()[deviceNum]
+    '''
+    Initializes OceanInsight device and obtains spectrum data using python-seabreeze library.
+    
+    Inherits Spectrometer class from seabreeze.
+    
+    Attributes
+    ----------
+    trigVal (int): Sets trigger mode. See setTriggerMode() method.
+    intTime (int): Integration time in microseconds.
+    deviceNum (int): Sets which device you want to use if there are multiple. If there is only one, then choose 0.
+    '''
+    def __init__(self, trig_val, int_time, device_num):
+        '''
+        Constructor for SpecInfo class.
+        
+        Parameters
+        ----------
+        trigVal (int): Sets trigger mode. See setTriggerMode() method.
+        intTime (int): Integration time in microseconds.
+        deviceNum (int): Sets which device you want to use if there are multiple. If there is only one, then choose 0.
+        '''
+        device = list_devices()[device_num]
         Spectrometer.__init__(self, device)
         self.spectrometer = Spectrometer(device)
-        self.spectrumData = np.array([[]])
-        self.trigVal = trigVal
-        self.intTime = intTime
+        self.spectrum_data = np.array([[]])
+        self.trig_val = trig_val
+        self.int_time = int_time
     
-    # Sets the trigger mode, which are as follows:
-    # normal = 0
-    # level/software = 1
-    # synchronization = 2
-    # edge/hardware = 3
-    def setTriggerMode(self):
+    def __set_trigger_mode(self):
+        ''' 
+        This function sets the trigger mode.
+        
+        A specific trigger mode can be set with the following values:
+        normal = 0
+        level/software = 1
+        synchronization = 2
+        edge/hardware = 3 
+        '''
         spec = self.spectrometer
-        spec.trigger_mode(self.trigVal)
+        spec.trigger_mode(self.trig_val)
     
     # Sets integration time in microseconds
-    def setIntTime(self):
-        self.spectrometer.integration_time_micros(self.intTime)
+    def __set_int_time(self):
+        '''
+        This function sets the integration time in microseconds.
+        '''
+        spec = self.spectrometer
+        spec.integration_time_micros(self.int_time)
     
     # Gets spectrometer data using seabreeze's spectrum() function
-    def getSpec(self):
-        spec = self.spectrometer
-        self.spectrumData = spec.spectrum()
-        return self.spectrumData
+    def get_spec(self):
+        '''
+        This function obtains data (intensity and wavelength) from the spectrometer.
         
-    def getShape(self):
-        shape = self.spectrumData.shape
+        The data is obtained using seabreeze's spectrum() function.
+        
+        Returns
+        -------
+        spectrumData: <numpy.ndarray>
+            2D array that contains wavelengths and intensities.
+        '''
+        spec = self.spectrometer
+        self.spectrum_data = spec.spectrum()
+        return self.spectrum_data
+        
+    def get_shape(self):
+        '''
+        Gets the shape of the 2D numpy array for the spectrum.
+
+        Returns
+        -------
+        shape: <tuple>
+        '''
+        shape = self.spectrum_data.shape
         return shape
     
     # Compresses spectrometer data for easy sending
-    def getSpecCompressed(self):
-        self.specCompressed = zlib.compress(self.spectrumData.tobytes())
-        return self.specCompressed
+    def get_spec_compressed(self):
+        '''
+        Compresses spectrum data from getSpec() for easy sending.
+
+        Returns
+        -------
+        specCompressed: <bytes>
+        '''
+        spec_compressed = zlib.compress(self.spectrum_data.tobytes())
+        return spec_compressed
